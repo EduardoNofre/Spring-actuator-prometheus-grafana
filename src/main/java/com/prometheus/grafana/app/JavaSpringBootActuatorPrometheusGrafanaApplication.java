7e78765c3654abb1ac804b20fa41d84ac347ff78
EDUARDO NOFRE REIS DE SA <eduardo.sa@gps-pamcary.com.br>
2021-04-30 13:01:48 -0300

    primeiro commit da aplicação

---
+++ src/main/java/com/prometheus/grafana/app/JavaSpringBootActuatorPrometheusGrafanaApplication.java
@@ -0,0 +1,13 @@
+package com.prometheus.grafana.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootActuatorPrometheusGrafanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootActuatorPrometheusGrafanaApplication.class, args);
+	}
+
+}
