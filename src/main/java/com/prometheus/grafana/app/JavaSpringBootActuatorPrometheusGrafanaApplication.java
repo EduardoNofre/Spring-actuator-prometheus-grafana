6e2e30e0f312b6fcf01d2e2131712a41bd111210
Eduardo Sa <nofrereis@gmail.com>
2021-05-04 23:32:35 -0300

    grafana e promethues sincronixados

--- src/main/java/com/prometheus/grafana/app/JavaSpringBootActuatorPrometheusGrafanaApplication.java
+++ src/main/java/com/prometheus/grafana/app/JavaSpringBootActuatorPrometheusGrafanaApplication.java
@@ -7,11 +7,10 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Micro Service Actuator, Prometheus e Grafana application exemplo", version = "1.0",description = " Estudo rapido "))
+@OpenAPIDefinition(info = @Info(title = "Micro Service Actuator, Prometheus e Grafana application exemplo", version = "1.0", description = " Estudo rapido "))
 public class JavaSpringBootActuatorPrometheusGrafanaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaSpringBootActuatorPrometheusGrafanaApplication.class, args);
 	}
-
 }
